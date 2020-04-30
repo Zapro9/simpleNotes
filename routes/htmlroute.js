a86548402a6392585fdea775d7d500cfd2c4cece
Zach Proctor <z.proctor@hotmail.com>
2020-04-30 10:20:09 -0600

    Further updates

--- routes/htmlroute.js
+++ routes/htmlroute.js
@@ -2,11 +2,11 @@ var path = require("path");
 var routes = require("express").Router();
 
 routes.get("/notes", function (req, res) {
-
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
 });
 
 routes.get("*", function (req, res) {
-
+ res.sendFile(path.join(__dirname, "../public/index.html"))
 });
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -8,8 +8,11 @@ var PORT = process.env.PORT || 3000
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-require("./routes/apiroute")(app);
-require("./routes/htmlroute")(app);
+// app.use(require('./routes/adminportal/home.js'));
+app.use("/api", require("./routes/apiroute.js"));
+app.use("/", require("./routes/htmlroute.js"));
+// require("./routes/apiroute.js");
+// require("./routes/htmlroute.js");
 
 
 app.use(express.static("public"));
