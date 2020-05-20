112504c3d1896c4fcd6fb739ff568c0557a243c9
Zach Proctor <z.proctor@hotmail.com>
2020-05-19 21:36:08 -0600

    more changes to server as well as heroku push

--- server.js
+++ server.js
@@ -8,14 +8,12 @@ var PORT = process.env.PORT || 3000
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-// app.use(require('./routes/adminportal/home.js'));
+
 app.use("/api", require("./routes/apiroute.js"));
 app.use("/", require("./routes/htmlroute.js"));
-// require("./routes/apiroute.js");
-// require("./routes/htmlroute.js");
 
 
-app.use(express.static("public"));
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
